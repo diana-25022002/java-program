7c4d139c18efacfb2613b22b104aa81f8e85deec
diana-25022002 <2502diana2002@gmail.com>
2022-07-08 13:46:14 +0530

    first commit

---
+++ demo.java
@@ -0,0 +1,6 @@
+class simple  {
+            public static void main(String args[]) {
+            System.out.println("hello java");
+            System.out.println("our first java 17 program");
+}
+            }
